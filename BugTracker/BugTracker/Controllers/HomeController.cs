4670a0f44d6f2e6ab518e884df616ede6770a658
Nour Mohammed <noureldinkhedr85@gmail.com>
2019-05-01 10:50:14 -0500

    Dashboard

--- BugTracker/BugTracker/Controllers/HomeController.cs
+++ BugTracker/BugTracker/Controllers/HomeController.cs
@@ -10,11 +10,86 @@ namespace BugTracker.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext DbContext;
+
+
+        public HomeController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
         [Authorize]
         public ActionResult Index()
         {
+            //Quering data to display on the Dashboard
+            var userId = User.Identity.GetUserId();
+
+            var currentUser = DbContext.Users.FirstOrDefault(
+                   user => user.Id == userId);
+
+            if (User.IsInRole("Admin") || User.IsInRole("Project Manager"))
+            {
+                var allProjects = (from project in DbContext.Projects
+                                   select project
+                             ).ToList();
+
+                ViewBag.AllProjects = allProjects.Count;
+
+                var openTickets = (from ticket in DbContext.Tickets
+                                   where ticket.TicketStatus.Name == "Open"
+                                   select ticket
+                                ).ToList();
+
+                var resolvedTickets = (from ticket in DbContext.Tickets
+                                       where ticket.TicketStatus.Name == "Resolved"
+                                       select ticket
+                               ).ToList();
+
+                var rejectedTickets = (from ticket in DbContext.Tickets
+                                       where ticket.TicketStatus.Name == "Rejected"
+                                       select ticket
+                              ).ToList();
+
+                ViewBag.AllProjects = allProjects.Count;
+                ViewBag.AllOpenTickets = openTickets.Count;
+                ViewBag.AllResolvedTickets = resolvedTickets.Count;
+                ViewBag.AllRejectedTickets = rejectedTickets.Count;
+            }
+
+
+            if (User.IsInRole("Developer"))
+            {
+                var developerAssignedProjects = (from project in currentUser.Projects
+                                                 select project
+                                                ).ToList();
+
+                var developerAssignedTickets = (from ticket in DbContext.Tickets
+                                                where ticket.AssignedToId == userId
+                                                select ticket
+                                               ).ToList();
+
+                ViewBag.CurrentDeveloperAssignedProjects = developerAssignedProjects.Count;
+
+                ViewBag.CurrentDeveloperAssignedTickets = developerAssignedTickets.Count;
+            }
+
+            if (User.IsInRole("Submitter"))
+            {
+                var submitterAssignedProjects = (from project in currentUser.Projects
+                                                 select project
+                                                ).ToList();
+
+                var submitterCreatedTickets = (from ticket in DbContext.Tickets
+                                                where ticket.CreatedById == userId
+                                                select ticket
+                                              ).ToList();
+
+                ViewBag.CurrentSubmitterAssignedProjects = submitterAssignedProjects.Count;
+                ViewBag.CurrentSubmitterCreatedTickets = submitterCreatedTickets.Count;
+            }
+
             return View();
-        }  
+        }
 
     }
 }
